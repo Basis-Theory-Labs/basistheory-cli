6a80b965961dcb9f52d3255320ccf80b3cd435c8
Davi Aquino <djejaquino@gmail.com>
2023-07-04 11:19:40 -0300

    fix: add logs ack

--- src/logs/parse.ts
+++ src/logs/parse.ts
@@ -11,46 +11,47 @@ interface LogEvent {
 
 const debug = require('debug')('logs:parse');
 
-const parseLogEvent =
-  (logger: pino.Logger) =>
-  async (event: LogEvent): Promise<void> => {
-    debug(event);
+const parseLogEvent = async (
+  logger: pino.Logger,
+  event: LogEvent
+): Promise<void> => {
+  debug(event);
 
-    const { deserializeError } = await import('serialize-error');
+  const { deserializeError } = await import('serialize-error');
 
-    const [arg1, arg2, ...args] = event.args.map((arg) => {
-      // eslint-disable-next-line unicorn/no-null
-      if (arg === null || arg === undefined || typeof arg === 'number') {
-        return String(arg);
-      }
-
-      if (arg?.name && arg?.stack) {
-        return deserializeError(arg);
-      }
-
-      return arg;
-    });
-
-    switch (event.level) {
-      case 'INFO': {
-        logger.info(arg1, arg2, ...args);
-
-        break;
-      }
-
-      case 'WARN': {
-        logger.warn(arg1, arg2, ...args);
-
-        break;
-      }
-
-      case 'ERROR': {
-        logger.error(arg1, arg2, ...args);
-
-        break;
-      }
-      // No default
+  const [arg1, arg2, ...args] = event.args.map((arg) => {
+    // eslint-disable-next-line unicorn/no-null
+    if (arg === null || arg === undefined || typeof arg === 'number') {
+      return String(arg);
     }
-  };
+
+    if (arg?.name && arg?.stack) {
+      return deserializeError(arg);
+    }
+
+    return arg;
+  });
+
+  switch (event.level) {
+    case 'INFO': {
+      logger.info(arg1, arg2, ...args);
+
+      break;
+    }
+
+    case 'WARN': {
+      logger.warn(arg1, arg2, ...args);
+
+      break;
+    }
+
+    case 'ERROR': {
+      logger.error(arg1, arg2, ...args);
+
+      break;
+    }
+    // No default
+  }
+};
 
 export { parseLogEvent };
