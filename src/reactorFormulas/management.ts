76427d70af888b37e17f84d85f88382a814c797a
James Armstead <james@basistheory.com>
2023-08-03 10:04:53 -0500

    featat: allowing reactor formulas to be updated

---
+++ src/reactorFormulas/management.ts
@@ -0,0 +1,86 @@
+import type {
+  ReactorFormula,
+  CreateReactorFormula,
+} from '@basis-theory/basis-theory-js/types/models';
+import type {
+  BasisTheory as IBasisTheory,
+  PaginatedList,
+} from '@basis-theory/basis-theory-js/types/sdk';
+import { ux } from '@oclif/core';
+import type { TableRow } from '../types';
+import { selectOrNavigate } from '../utils';
+
+const debug = require('debug')('reactor-formulas:management');
+
+const listReactorFormulas = async (
+  bt: IBasisTheory,
+  page: number
+): Promise<PaginatedList<ReactorFormula>> => {
+  const size = 5;
+
+  debug('Listing reactors', `page: ${page}`, `size: ${size}`);
+
+  const reactorFormulas = await bt.reactorFormulas.list({
+    size,
+    page,
+  });
+
+  let data: TableRow<ReactorFormula>[] = [];
+
+  if (reactorFormulas.pagination.totalItems > 0) {
+    const last = (reactorFormulas.pagination.pageNumber - 1) * size;
+
+    data = reactorFormulas.data.map((reactorFormulas, index) => ({
+      '#': last + (index + 1),
+      ...reactorFormulas,
+    }));
+
+    ux.table(data, {
+      '#': {},
+      id: {},
+      name: {},
+    });
+  } else {
+    ux.log('No reactor formulas found.');
+  }
+
+  return {
+    data,
+    pagination: reactorFormulas.pagination,
+  };
+};
+
+const selectReactorFormula = async (
+  bt: IBasisTheory,
+  page: number
+): Promise<ReactorFormula | undefined> => {
+  const reactors = await listReactorFormulas(bt, page);
+
+  if (reactors.pagination.totalItems === 0) {
+    return undefined;
+  }
+
+  const selection = await selectOrNavigate(reactors, '#');
+
+  if (selection === 'previous') {
+    return selectReactorFormula(bt, page - 1);
+  }
+
+  if (selection === 'next') {
+    return selectReactorFormula(bt, page + 1);
+  }
+
+  return selection;
+};
+
+const updateReactorFormula = (
+  bt: IBasisTheory,
+  id: string,
+  model: CreateReactorFormula
+): Promise<ReactorFormula> => {
+  debug(`Patching Reactor ${id}`, JSON.stringify(model, undefined, 2));
+
+  return bt.reactorFormulas.update(id, model);
+};
+
+export { selectReactorFormula, updateReactorFormula };
