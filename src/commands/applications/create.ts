0ddf69dc57f5cc2e262966507a689d4485630a5d
Davi Aquino <djejaquino@gmail.com>
2023-12-11 15:34:53 -0300

    feat: support application templates

--- src/commands/applications/create.ts
+++ src/commands/applications/create.ts
@@ -5,7 +5,9 @@ import {
 import { Flags } from '@oclif/core';
 import {
   createApplication,
+  createApplicationFromTemplate,
   listPermissions,
+  promptTemplate,
 } from '../../applications/management';
 import { APPLICATION_FLAGS } from '../../applications/utils';
 import { BaseCommand } from '../../base';
@@ -28,39 +30,67 @@ export default class Create extends BaseCommand {
       description: 'type of the Application',
       options: [...APPLICATION_TYPES],
     }),
+    template: Flags.string({
+      description: 'template ID to create the application with',
+      char: 'z',
+    }),
   };
 
   public async run(): Promise<void> {
     const { flags, bt } = await this.parse(Create);
 
-    const name = await promptStringIfUndefined(flags.name, {
-      message: 'What is the Application name?',
-      validate: (value) => Boolean(value),
-    });
+    let application;
 
-    const type = (await promptSelectIfUndefined(flags.type, {
-      message: 'What is the Application type?',
-      choices: APPLICATION_TYPES.map((t) => ({ value: t })),
-    })) as ApplicationType;
+    if (flags.template) {
+      // template passed as flag
+      application = await createApplicationFromTemplate(bt, flags.template);
+    } else {
+      let template;
+      let rules, permissions, type;
 
-    const available = await listPermissions(bt, type);
+      if (!flags.name && !flags.type && !flags.permission) {
+        // no other flags where passed, prompt template
+        template = await promptTemplate(bt);
+      }
 
-    const permissions = await promptCheckboxIfUndefined(flags.permission, {
-      message: 'Select the permissions for the Application',
-      choices: available.map((p) => ({
-        value: p.type,
-      })),
-      loop: false,
-    });
+      const name = await promptStringIfUndefined(flags.name, {
+        message: 'What is the Application name?',
+        default: template?.name,
+        validate: (value) => Boolean(value),
+      });
 
-    const { id, key } = await createApplication(bt, {
-      name,
-      type,
-      permissions,
-    });
+      if (template) {
+        // template has been picked, so type and permissions/rules are defined
+        type = template.applicationType;
+        rules = template.rules;
+        permissions = template.permissions;
+      } else {
+        type = (await promptSelectIfUndefined(flags.type, {
+          message: 'What is the Application type?',
+          choices: APPLICATION_TYPES.map((t) => ({ value: t })),
+        })) as ApplicationType;
+
+        const available = await listPermissions(bt, type);
+
+        permissions = await promptCheckboxIfUndefined(flags.permission, {
+          message: 'Select the permissions for the Application',
+          choices: available.map((p) => ({
+            value: p.type,
+          })),
+          loop: false,
+        });
+      }
+
+      application = await createApplication(bt, {
+        name,
+        type,
+        permissions,
+        rules,
+      });
+    }
 
     this.log('Application created successfully!');
-    this.log(`id: ${id}`);
-    this.log(`key: ${key}`);
+    this.log(`id: ${application.id}`);
+    this.log(`key: ${application.key}`);
   }
 }
